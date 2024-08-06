71885f9141028f311d94f9951b89ac66f8700e72
Ikuo-Matsutori <135724892+Ikuo-Matsutori@users.noreply.github.com>
2024-08-06 16:03:28 +0900

    add:loging

--- src/main/kotlin/com/example/todoApp/DefaultTodoService.kt
+++ src/main/kotlin/com/example/todoApp/DefaultTodoService.kt
@@ -3,14 +3,17 @@ package com.example.todoApp
 import com.example.todoApp.dto.NewTodoRequest
 import com.example.todoApp.dto.TodoResponse
 import com.example.todoApp.dto.UpdateTodoRequest
+import org.slf4j.LoggerFactory
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
 class DefaultTodoService(val todoRepository: TodoRepository) : TodoService {
+    val log = LoggerFactory.getLogger(DefaultTodoService::class.java)
+
     override fun todos(): List<TodoResponse> {
 
-        val todoEntities = todoRepository.findAll() // List<TodoEntity>
+        val todoEntities = todoRepository.findAll()
         return todoEntities.map {
             TodoResponse(it.id, it.text)
         }
@@ -23,8 +26,17 @@ class DefaultTodoService(val todoRepository: TodoRepository) : TodoService {
     }
 
     override fun update(id: Long, updateTodoRequest: UpdateTodoRequest): TodoResponse {
-        val foundTodo: TodoEntity =
-            todoRepository.findByIdOrNull(id) ?: throw RuntimeException("no todo found for id $id")
+        val foundTodo: TodoEntity? = todoRepository.findByIdOrNull(id)
+
+        if (foundTodo == null) {
+            val err = "no todo found for id $id"
+//            log.trace(err)
+//            log.debug(err)
+//            log.info(err)
+//            log.warn(err)
+            log.error(err)
+            throw RuntimeException(err)
+        }
 
         val updated = todoRepository.save(TodoEntity(foundTodo.id, updateTodoRequest.text))
         return TodoResponse(updated.id, updated.text)
--- src/main/kotlin/com/example/todoApp/TodoController.kt
+++ src/main/kotlin/com/example/todoApp/TodoController.kt
@@ -3,10 +3,7 @@ package com.example.todoApp
 import com.example.todoApp.dto.NewTodoRequest
 import com.example.todoApp.dto.TodoResponse
 import com.example.todoApp.dto.UpdateTodoRequest
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.data.jpa.domain.AbstractPersistable_.id
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -25,17 +22,18 @@ class TodoController(val todoService: TodoService) {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun postTodo(@RequestBody newTodo: NewTodoRequest) :Long{
+    fun postTodo(@RequestBody newTodo: NewTodoRequest): Long {
         return todoService.create(newTodo)
     }
 
     @PutMapping("/{id}")
-    fun updateTodo(@PathVariable id: Long ,@RequestBody updateTodoRequest: UpdateTodoRequest): TodoResponse {
+    fun updateTodo(@PathVariable id: Long, @RequestBody updateTodoRequest: UpdateTodoRequest): TodoResponse {
         return todoService.update(id, updateTodoRequest)
 
     }
+
     @DeleteMapping("/{id}")
-    fun deleteTodo(@PathVariable id: Long):Long{
+    fun deleteTodo(@PathVariable id: Long): Long {
         return todoService.delete(id)
     }
 }
